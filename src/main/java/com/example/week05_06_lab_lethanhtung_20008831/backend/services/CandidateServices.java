7023409c6501787ffee7d2af5df58ea0ab0c6168
tungcutte35 <booking.tungcutte@gmail.com>
2023-10-20 20:39:10 +0700

    add services

---
+++ src/main/java/com/example/week05_06_lab_lethanhtung_20008831/backend/services/CandidateServices.java
@@ -0,0 +1,5 @@
+package com.example.week05_06_lab_lethanhtung_20008831.backend.services;
+
+import com.example.week05_06_lab_lethanhtung_20008831.backend.models.Candidate;
+
+public interface CandidateServices extends ParentServices<Candidate> { }
