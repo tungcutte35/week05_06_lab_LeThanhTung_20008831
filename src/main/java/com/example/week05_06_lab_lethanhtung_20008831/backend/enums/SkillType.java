14238cf1969021ecf1e36f5e532c3aa87ae00fdd
tungcutte35 <booking.tungcutte@gmail.com>
2023-11-24 18:05:42 +0700

    enums

--- src/main/java/com/example/week05_06_lab_lethanhtung_20008831/backend/enums/SkillType.java
+++ src/main/java/com/example/week05_06_lab_lethanhtung_20008831/backend/enums/SkillType.java
@@ -2,8 +2,9 @@ package com.example.week05_06_lab_lethanhtung_20008831.backend.enums;
 
 public enum SkillType {
     UNSPECITFIC(0),
-    SOFT_SKILL(1),
-    TECHNICAL_SKILL(2);
+    TECHNICAL_SKILL(1),
+    SOFT_SKILL(2);
+
     private int value;
     SkillType(int value){
         this.value = value;
