bb94e3f8cff7d04ee376fbbeba6079708a3d53f6
tungcutte35 <booking.tungcutte@gmail.com>
2023-11-24 18:07:02 +0700

    resource and services

--- src/main/java/com/example/week05_06_lab_lethanhtung_20008831/backend/resource/CandidateResources.java
+++ src/main/java/com/example/week05_06_lab_lethanhtung_20008831/backend/resource/CandidateResources.java
@@ -1,4 +1,18 @@
 package com.example.week05_06_lab_lethanhtung_20008831.backend.resource;
 
+import com.example.week05_06_lab_lethanhtung_20008831.backend.services.CandidateServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/${app.rest.path}")
 public class CandidateResources {
+    @Autowired
+    private CandidateServices services;
+//    @GetMapping(path = "/candidate")
+//    public String beat(){
+//        return "OK ưef";
+//    }
+
 }
--- src/main/java/com/example/week05_06_lab_lethanhtung_20008831/backend/services/CandidateServices.java
+++ src/main/java/com/example/week05_06_lab_lethanhtung_20008831/backend/services/CandidateServices.java
@@ -1,5 +1,37 @@
 package com.example.week05_06_lab_lethanhtung_20008831.backend.services;
 
 import com.example.week05_06_lab_lethanhtung_20008831.backend.models.Candidate;
+import com.example.week05_06_lab_lethanhtung_20008831.backend.respositories.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
 
-public interface CandidateServices extends ParentServices<Candidate> { }
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class CandidateServices {
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public Page<Candidate> findAll(int pageNo, int pageSize, String sortBy, String sortDirection) {
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        return candidateRepository.findAll(pageable);//findFirst.../findTop...
+    }
+
+    public Page<Candidate> findPaginated(Pageable pageable) {
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List<Candidate> list;
+        List<Candidate> candidates = candidateRepository.findAll();
+        if (candidates.size() < startItem) {
+            list = Collections.emptyList();
+        } else {
+            int toIndex = Math.min(startItem + pageSize, candidates.size());
+            list = candidates.subList(startItem, toIndex);
+        }
+        return new PageImpl<>(list, PageRequest.of(currentPage, pageSize), candidates.size());
+    }
+}
